66a7168edb3cf8670ee8e4d5ac98dc34034415b3
NazariiPav1iuk <77924548+NazariiPav1iuk@users.noreply.github.com>
2021-07-02 13:54:01 +0300

    2commit

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
+    // Slick slider
     $('.profitability-slider').slick({
         centerMode: false,
-        centerPadding: '60px',
         slidesToShow: 4,
+        arrows: false,
         responsive: [
         {
             breakpoint: 1024,
@@ -27,6 +28,38 @@ $(document).ready(function(){
             settings: {
               arrows: false,
               centerMode: true,
+              centerPadding: '30px',
+              slidesToShow: 1,
+              dots:true
+            }
+          }
+        ]
+      });
+
+      $('.working-slider').slick({
+        slidesToShow: 3,
+        responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                arrows: false,
+                slidesToShow: 2,
+                dots:true
+            }
+            },
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              slidesToShow: 2,
+              dots:true
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: false,
               centerPadding: '30px',
               slidesToShow: 1,
               dots:true
